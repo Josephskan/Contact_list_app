4ad5cdcdd92103944334599cc1e183c556e34649
josephskan <josephskan@live.com>
2016-01-12 20:49:26 -0500

    done all mandatory work... YAY

--- contact.rb
+++ contact.rb
@@ -20,71 +20,57 @@ class Contact
 
     # Returns an Array of Contacts loaded from the database.
     def all
-      puts "You have selected list"
       count = 0
+      contact_info = ''
       # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
       CSV.foreach('./contacts.csv') do |row|
         line = "#{row[0]}, (#{row[1]})"
         count += 1
-        puts "#{count}: #{line}"
+        contact_info << "\n#{count}: #{line}"
       end
-      puts"-------------------------------------------------------"
-      puts "#{count} contacts on record"
-      puts
-      # binding.pry
+      contact_info
     end
 
     # Creates a new contact, adding it to the database, returning the new contact.
     def create(name, email)
       Contact.new(name, email)
-
       # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
     end
 
     # Returns the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
+      count = 1
+      contact = ''
+      id = id.to_i
+      CSV.foreach('./contacts.csv') do |row|
+        if count == (id.to_i)
+          return contact = "#{row[0]}, (#{row[1]})"
+        end
+          count += 1
+      end
+
+      if contact == ''
+        contact = "Contact DNE, sorry"
+      end
+      contact
       # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
     end
 
     # Returns an array of contacts who match the given term.
     def search(term)
+      listing = ''
+      CSV.foreach('./contacts.csv') do |row|
+        line = "#{row[0]}, (#{row[1]})"
+        if line.match(/.*#{term}.*/)  
+          listing << "\n#{row[0]}, (#{row[1]})"
+        end
+      end
+      if listing == ''
+        listing = "No matches to your keyword."
+      end
+      listing
       # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
     end
-
-    def start_up
-    puts
-    puts "Here is a list of available commands:"
-    puts
-    puts "\tnew - Create a new contact"
-    puts
-    puts "\tlist - List all contacts"
-    puts
-    puts "\tshow - Show a contact"
-    puts
-    puts "\tsearch - Search contacts"
-    puts
-
-    cmd = gets.chomp
-    case cmd
-    when "new"
-      puts "please enter contact full name:"
-      name = gets.chomp
-      puts "now enter contact's email"
-      email = gets.chomp
-      self.create(name, email)
-    when "list"
-      self.all
-    when "show"
-      self.find()
-    when "search"
-      self.search()
-    end
   end
-
-  end
-
-  
-
-  Contact.start_up
 
 end
--- contact_list.rb
+++ contact_list.rb
@@ -5,4 +5,46 @@ class ContactList
 
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
 
+  def self.menu_interface
+    puts
+    puts "Here is a list of available commands:"
+    puts
+    puts "\tnew - Create a new contact"
+    puts
+    puts "\tlist - List all contacts"
+    puts
+    puts "\tshow - Show a contact"
+    puts
+    puts "\tsearch - Search contacts"
+    puts
+
+    cmd = gets.chomp
+    if /new/ =~ cmd
+      puts "Please enter contact full name:"
+      name = gets.chomp
+      puts "Now enter contact's email"
+      email = gets.chomp
+      Contact.create(name, email)
+    elsif /list/ =~ cmd
+      puts Contact.all
+
+      row_num = 0
+      CSV.foreach('./contacts.csv') do |row|
+        row_num += 1
+      end
+      puts "-------------------------------------------------------"
+      puts "#{row_num} contacts on record"
+      puts
+    elsif /show/ =~ cmd
+      puts "Enter an ID:" 
+      id = gets.chomp
+      puts Contact.find(id)
+    elsif /search/ =~ cmd
+      puts "enter a search keyword"
+      keyword = gets.chomp
+      puts Contact.search(keyword)
+    end
+  end
+
+ContactList.menu_interface
 end
